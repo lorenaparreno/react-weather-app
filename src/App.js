3f2d25cff615fbedf73dcf5b7d8d63ce60eb8ec8
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-04-26 22:27:33 -0400

    Add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>This project was coded by {""}
+        <a href="https://github.com/lorenaparreno" target="_blank">Lorena Parreno</a>{""} and is open-sourced on {""}
+        <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank">GitHub</a>
+      </footer>
     </div>
   );
 }
