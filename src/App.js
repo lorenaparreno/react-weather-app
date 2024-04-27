b838bd3a722387f614fb90bbd5397c1077bda807
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-04-26 23:37:24 -0400

    Add Weather.js and Weather.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+
+        <Weather />
         <footer>This project was coded by {""}
           <a href="https://github.com/lorenaparreno" target="_blank">Lorena Parreno</a>{""} and is open-sourced on {""}
           <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank">GitHub</a>
