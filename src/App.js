99ef05de60d4da51b7facf9e9acddbc889bb7586
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-04-26 22:54:23 -0400

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>This project was coded by {""}
-        <a href="https://github.com/lorenaparreno" target="_blank">Lorena Parreno</a>{""} and is open-sourced on {""}
-        <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank">GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>This project was coded by {""}
+          <a href="https://github.com/lorenaparreno" target="_blank">Lorena Parreno</a>{""} and is open-sourced on {""}
+          <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
