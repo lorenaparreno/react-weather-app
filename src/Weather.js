ed9e0cf38f8fade2fc1d5b6a50d16c5d70d4e575
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-05-03 00:25:29 -0400

    Add WeatherInfo.js and use the correct icon URL and description

--- src/Weather.js
+++ src/Weather.js
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
+    const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
         setWeatherData({
             ready: true,
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
-            description: "Clear Sky",
-            iconUrl: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png",
+            description: response.data.weather[0].description,
+            iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}`,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed
         });
     }
 
+    function search() {
+        const apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
+    }
+
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
                             <input
@@ -29,6 +45,7 @@ export default function Weather(props) {
                                 placeholder="Enter a city"
                                 className="form-control"
                                 autoFocus="on"
+                                onChange={handleCityChange}
                             />
                         </div>
                         <div className="col-3">
@@ -36,38 +53,12 @@ export default function Weather(props) {
                         </div>
                     </div>
                 </form>
-                <h1>{weatherData.city}</h1>
-                <ul>
-                    <li><FormattedDate date={weatherData.date} /></li>
-                    <li className="text-capitalize">{weatherData.description}</li>
-                </ul>
-                <div className="row mt-3">
-                    <div className="col-6">
-                        <div className="clearfix">
-                            <img
-                                src={weatherData.iconUrl}
-                                alt={weatherData.description}
-                                className="float-left"
-                            />
-                            <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                            <span className="unit">℃</span>
-                        </div>
-                    </div>
-                    <div className="col-6">
-                        <ul>
-                            <li>Humidity: {weatherData.humidity}%</li>
-                            <li>Wind: {Math.round(weatherData.wind)}km/h</li>
-                        </ul>
-                    </div>
-                </div>
+                <WeatherInfo data={weatherData} />
+
             </div>
         );
     } else {
-        const apiKey = "6d68aadfacdd4f5163bc273049a0cf2d";
-        let city = "New York";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        axios.get(apiUrl).then(handleResponse);
-
+        search();
         return "Loading...";
     }
 }
