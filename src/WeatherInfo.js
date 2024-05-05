360cb5990b80233f723c3f49dd3c25485c18837d
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-05-05 18:33:06 -0400

    Edit icon description. Create WeatherTemperature.js component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,31 +1,36 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
+    const { data } = props;
+
+    if (!data || !data.ready) {
+        return null; // or you can render a loading state
+    }
+
     return (
         <div className="WeatherInfo">
-            <h1>{props.data.city}</h1>
+            <h1>{data.city}</h1>
             <ul>
-                <li><FormattedDate date={props.data.date} /></li>
-                <li className="text-capitalize">{props.data.description}</li>
+                <li><FormattedDate date={data.date} /></li>
+                <li className="text-capitalize">{data.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="weather-icon-temp">
-                        <WeatherIcon code={props.data.icon} />
+                        {data.icon && <WeatherIcon code={data.icon} />}
+
                         <div className="temperature-container mt-1">
-                            <span className="temperature">
-                                {Math.round(props.data.temperature)}
-                            </span>
-                            <span className="unit">℃</span>
+                            <WeatherTemperature celsius={props.data.temperature} />
                         </div>
                     </div>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Humidity: {props.data.humidity}%</li>
-                        <li>Wind: {Math.round(props.data.wind)}km/h</li>
+                        <li>Humidity: {data.humidity}%</li>
+                        <li>Wind: {Math.round(data.wind)}km/h</li>
                     </ul>
                 </div>
             </div>
