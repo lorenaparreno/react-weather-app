8d2580314bcaa20f82d7fb98b100128bdf91f347
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-05-05 18:42:29 -0400

    Add link hosting it on Netlify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default function App() {
         <Weather defaultCity="New York" />
         <footer>This project was coded by {""}
           <a href="https://github.com/lorenaparreno" target="_blank" className="custom-link" rel="noreferrer">Lorena Parreno</a>{""} and is open-sourced on {""}
-          <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank" className="custom-link" rel="noreferrer">GitHub</a>
+          <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank" className="custom-link" rel="noreferrer">GitHub</a>{""} and is hosted on {""}
+          <a href="https://react-weather-app-homework-wk5.netlify.app/" target="_blank" className="custom-link" rel="noreferrer">Netlify</a>.
         </footer>
       </div>
     </div>
