de56ae1b7339daac438c0802f19b5115ae9bee98
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-05-12 17:34:53 -0400

    Update the current Netlify link.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         <footer className="mt-2">This project was coded by {""}
           <a href="https://github.com/lorenaparreno" target="_blank" className="custom-link" rel="noreferrer">Lorena Parreno</a>{""} and is open-sourced on {""}
           <a href="https://github.com/lorenaparreno/react-weather-app" target="_blank" className="custom-link" rel="noreferrer">GitHub</a>{""} and is hosted on {""}
-          <a href="https://react-weather-app-homework-wk5.netlify.app/" target="_blank" className="custom-link" rel="noreferrer">Netlify</a>.
+          <a href="https://react-weather-app-final-project-week6.netlify.app/" target="_blank" className="custom-link" rel="noreferrer">Netlify</a>.
         </footer>
       </div>
     </div>
